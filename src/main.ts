ad29d9ac8aeb4b5cfe1f678ce881dd571567a6a0
jjanzen93 <132104176+jjanzen93@users.noreply.github.com>
2024-10-10 01:35:04 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,30 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Salami Selector";
+const gameName = "Steak Selector";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+function increaseScore(score: number) {
+    return score + 1;
+}
+
 const button = document.createElement("button");
-button.innerHTML = "&#128516";
+button.innerHTML = "&#129385";
+
+
+let num_steak: number = 0;
+
+const listen_div = document.createElement("div");
+listen_div.innerHTML = `You have made ${num_steak} steaks!`;
+app.append(listen_div);
+
+button.onclick = () => {
+    num_steak = increaseScore(num_steak);
+    listen_div.innerHTML = `You have made ${num_steak} steaks!`;
+}
 app.append(button);
+
