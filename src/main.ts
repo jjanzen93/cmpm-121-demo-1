3485cf36e0a457c5762900671bbb341af783d953
jjanzen93 <132104176+jjanzen93@users.noreply.github.com>
2024-10-11 01:43:00 -0700

    Step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,19 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-function increaseScore(score: number) {
-    return score + 1;
+function increaseScore(score: number, increase: number) {
+    return score + increase;
 }
 
-const button = document.createElement("button");
-button.innerHTML = "&#129385";
+const steak_button = document.createElement("button");
+steak_button.innerHTML = "&#129385";
+app.append(steak_button);
 
+const upgrade_cost = 10;
+const upgrade_button = document.createElement("button");
+upgrade_button.innerHTML = `Upgrade (Costs ${upgrade_cost} Steaks)`
+app.append(upgrade_button);
+upgrade_button.disabled = true;
 
 let num_steak: number = 0;
 
@@ -23,15 +29,32 @@ const listen_div = document.createElement("div");
 listen_div.innerHTML = `You have made ${num_steak} steaks!`;
 app.append(listen_div);
 
-function createSteak() {
-    num_steak = increaseScore(num_steak);
-    listen_div.innerHTML = `You have made ${num_steak} steaks!`;
+function createSteak(current_increase: number) {
+    num_steak = increaseScore(num_steak, current_increase);
+    listen_div.innerHTML = `You have made ${num_steak.toFixed(1)} steaks!`;
 }
 
-button.onclick = () => {
-    createSteak();
+steak_button.onclick = () => {
+    createSteak(1);
 }
 
-app.append(button);
+upgrade_button.onclick = () => {
+    num_steak -= upgrade_cost;
+}
 
-window.setInterval(createSteak, 1000);
+
+let last_time: number = performance.now();
+
+function update(timestamp: number) {
+    const time_elapsed: number = timestamp - last_time;
+    createSteak((time_elapsed/1000));
+    last_time = timestamp;
+    if (num_steak >= upgrade_cost) {
+        upgrade_button.disabled = false;
+    } else {
+        upgrade_button.disabled = true;
+    }
+    window.requestAnimationFrame(update);
+}
+
+window.requestAnimationFrame(update);
