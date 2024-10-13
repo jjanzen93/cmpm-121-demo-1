699158b8deea4d6fbebc6bdec241eceefaa7f627
jjanzen93 <132104176+jjanzen93@users.noreply.github.com>
2024-10-12 20:41:08 -0700

    Step 9 complete

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ function increaseScore(score: number, increase: number) {
     return score + increase;
 }
 let growth_factor = 0;
-let num_steak: number = 10000;
+let num_steak: number = 0;
 
 // make steak button
 const steak_button = document.createElement("button");
@@ -22,34 +22,34 @@ steak_button.innerHTML = "&#129385";
 app.append(steak_button);
 app.append(document.createElement("br"));
 
-// upgrade A information
-let a_upgrade_cost = 10;
-const a_growth_increase = 0.1;
-const a_upgrade_button = document.createElement("button");
-a_upgrade_button.innerHTML = `Upgrade Cow Feed (Costs ${a_upgrade_cost} Steaks)`;
-app.append(a_upgrade_button);
-app.append(document.createElement("br"));
-a_upgrade_button.disabled = true;
+interface Item {
+    name: string,
+    cost: number,
+    rate: number
+};
 
-// upgrade B information
-let b_upgrade_cost = 100;
-const b_growth_increase = 2;
-const b_upgrade_button = document.createElement("button");
-b_upgrade_button.innerHTML = `Upgrade Meat Quality (Costs ${b_upgrade_cost} Steaks)`;
-app.append(b_upgrade_button);
-app.append(document.createElement("br"));
-b_upgrade_button.disabled = true;
+interface Upgrade {
+    button: HTMLButtonElement,
+    amount: number
+};
+  
+const availableItems : Item[] = [
+    {name: "Buy Cow Farm", cost: 10, rate: 0.1},
+    {name: "Upgrade Slaughterhouse", cost: 100, rate: 2},
+    {name: "Build Packing Facility", cost: 1000, rate: 50}
+];
 
-// upgrade C information
-let c_upgrade_cost = 1000;
-const c_growth_increase = 50;
-const c_upgrade_button = document.createElement("button");
-c_upgrade_button.innerHTML = `Upgrade Packing Facility (Costs ${c_upgrade_cost} Steaks)`;
-app.append(c_upgrade_button);
-app.append(document.createElement("br"));
-c_upgrade_button.disabled = true;
+const buttons : Upgrade[] = [];
 
-
+// create buttons
+for (const element of availableItems) {
+    const upgrade : Upgrade = {button: document.createElement("button"), amount: 0};
+    buttons.push(upgrade);
+    upgrade.button.innerHTML = `${element.name} (You have ${upgrade.amount}.)`;
+    app.append(upgrade.button);
+    app.append(document.createElement("br"));
+    upgrade.button.disabled = true;
+};
 
 const num_div = document.createElement("div");
 num_div.innerHTML = `You have made ${num_steak} steaks!`;
@@ -58,65 +58,53 @@ app.append(num_div);
 function createSteak(current_increase: number) {
     num_steak = increaseScore(num_steak, current_increase);
     num_div.innerHTML = `You have made ${num_steak.toFixed(1)} steaks!`;
-}
+};
 
 // display current growth rate
 const growth_div = document.createElement("div");
 growth_div.innerHTML = `${growth_factor.toFixed(1)} steaks/sec`;
 app.append(growth_div);
 
-// button listeners
+// steak button listener
 steak_button.onclick = () => {
     createSteak(1);
-}
+};
 
-a_upgrade_button.onclick = () => {
-    num_steak -= a_upgrade_cost;
-    a_upgrade_cost *= 1.15;
-    a_upgrade_button.innerHTML = `Upgrade Cow Feed (Costs ${a_upgrade_cost} Steaks)`;
-    growth_factor += a_growth_increase;
-    growth_div.innerHTML = `${growth_factor.toFixed(1)} steaks/sec`;
-}
-
-b_upgrade_button.onclick = () => {
-    num_steak -= b_upgrade_cost;
-    b_upgrade_cost *= 1.15;
-    b_upgrade_button.innerHTML = `Upgrade Meat Quality (Costs ${b_upgrade_cost} Steaks)`;
-    growth_factor += b_growth_increase;
-    growth_div.innerHTML = `${growth_factor.toFixed(1)} steaks/sec`;
-}
-
-c_upgrade_button.onclick = () => {
-    num_steak -= c_upgrade_cost;
-    c_upgrade_cost *= 1.15;
-    c_upgrade_button.innerHTML = `Upgrade Packing Facility (Costs ${c_upgrade_cost} Steaks)`;
-    growth_factor += c_growth_increase;
-    growth_div.innerHTML = `${growth_factor.toFixed(1)} steaks/sec`;
-}
-
+// upgrade button listeners
+buttons.forEach(
+    (current_button) => {
+        const current_item = availableItems[buttons.indexOf(current_button)];
+        current_button.button.onclick = () => {
+            num_steak -= current_item.cost;
+            current_item.cost *= 1.15;
+            current_button.amount++;
+            current_button.button.innerHTML = `${current_item.name} (You have ${current_button.amount}.)`;
+            growth_factor += current_item.rate;
+            growth_div.innerHTML = `${growth_factor.toFixed(1)} steaks/sec`;
+        };
+    }
+);
 
 let last_time: number = performance.now();
 
 function update(timestamp: number) {
     const time_elapsed: number = timestamp - last_time;
+    // update score by growth rate
     createSteak((time_elapsed/1000)*growth_factor);
+    // update time
     last_time = timestamp;
-    if (num_steak >= a_upgrade_cost) {
-        a_upgrade_button.disabled = false;
-    } else {
-        a_upgrade_button.disabled = true;
-    }
-    if (num_steak >= b_upgrade_cost) {
-        b_upgrade_button.disabled = false;
-    } else {
-        b_upgrade_button.disabled = true;
-    }
-    if (num_steak >= c_upgrade_cost) {
-        c_upgrade_button.disabled = false;
-    } else {
-        c_upgrade_button.disabled = true;
-    }
+    // check item affordability
+    buttons.forEach(
+        (current_button) => {
+            const current_item = availableItems[buttons.indexOf(current_button)];
+            if (num_steak >= current_item.cost) {
+                current_button.button.disabled = false;
+            } else {
+                current_button.button.disabled = true;
+            };
+        }
+    );
     window.requestAnimationFrame(update);
-}
+};
 
 window.requestAnimationFrame(update);
